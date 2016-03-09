867cb3de0875dacd86a26639c69929c2acd30406
Vatsal Joshi <vatz88@gmail.com>
2016-03-09 10:39:19 +0530

    Excel online embed

---
+++ colorChange.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    var divBlock = document.getElementById('mySlot');
+    if (divBlock.backgroundColor.match("#FFFFCC")) {
+        divBlock.backgroundColor = "#CCFF33";
+    } else {
+        divBlock.backgroundColor = "#FFFFCC";
+    }
+}

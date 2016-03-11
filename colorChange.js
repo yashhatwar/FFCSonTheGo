b1edaffc815f6419ed57921fc4091e303d28060f
Vatsal Joshi <vatz88@gmail.com>
2016-03-11 13:33:26 +0530

    Slot tiles added with color change effect
    custom Metro UI used

--- colorChange.js
+++ colorChange.js
@@ -1,12 +1,143 @@
-function changeColor() {
-    var divBlock = document.getElementById('mySlot');
-    if (divBlock.backgroundColor.match("#FFFFCC")) {
-        divBlock.backgroundColor = "#CCFF33";
-    } else {
-        divBlock.backgroundColor = "#FFFFCC";
-    }
-}
-
 $(".TimetableContent").click(function () {
    $(this).toggleClass("highlight");
+});
+
+$(".A1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".A1").toggleClass("highlight");
+});
+
+$(".B1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".B1").toggleClass("highlight");
+});
+
+$(".C1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".C1").toggleClass("highlight");
+});
+
+$(".D1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".D1").toggleClass("highlight");
+});
+
+$(".E1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".E1").toggleClass("highlight");
+});
+
+$(".F1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".F1").toggleClass("highlight");
+});
+
+$(".G1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".G1").toggleClass("highlight");
+});
+
+$(".A2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".A2").toggleClass("highlight");
+});
+
+$(".B2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".B2").toggleClass("highlight");
+});
+
+$(".C2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".C2").toggleClass("highlight");
+});
+
+$(".D2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".D2").toggleClass("highlight");
+});
+
+$(".E2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".E2").toggleClass("highlight");
+});
+
+$(".F2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".F2").toggleClass("highlight");
+});
+
+$(".G2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".G2").toggleClass("highlight");
+});
+
+$(".TA1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TA1").toggleClass("highlight");
+});
+
+$(".TB1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TB1").toggleClass("highlight");
+});
+
+$(".TC1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TC1").toggleClass("highlight");
+});
+
+$(".TD1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TD1").toggleClass("highlight");
+});
+
+$(".TE1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TE1").toggleClass("highlight");
+});
+
+$(".TF1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TF1").toggleClass("highlight");
+});
+
+$(".TG1-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TG1").toggleClass("highlight");
+});
+
+$(".TA2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TA2").toggleClass("highlight");
+});
+
+$(".TB2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TB2").toggleClass("highlight");
+});
+
+$(".TC2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TC2").toggleClass("highlight");
+});
+
+$(".TD2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TD2").toggleClass("highlight");
+});
+
+$(".TE2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TE2").toggleClass("highlight");
+});
+
+$(".TF2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TF2").toggleClass("highlight");
+});
+
+$(".TG2-tile").click(function(){
+	$(this).toggleClass("highlight");
+	$(".TG2").toggleClass("highlight");
 });

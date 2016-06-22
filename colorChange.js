f8be3fe03e952713cc1b4ba51c0f0469a44acb8c
Vatsal Joshi <vatz88@gmail.com>
2016-06-22 12:52:48 +0530

    Color change of tile and timetable more optimized.

--- colorChange.js
+++ colorChange.js
@@ -2,7 +2,219 @@ $(".TimetableContent").click(function () {
    $(this).toggleClass("highlight");
 });
 
+//trial WORKS!!!
 $(".A1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".A1").removeClass("highlight");
+	}
+	else{
+		$(".A1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".A1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".A1-tile").removeClass("highlight");
+	}
+});
+
+$(".B1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".B1").removeClass("highlight");
+	}
+	else{
+		$(".B1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".B1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".B1-tile").removeClass("highlight");
+	}
+});
+
+$(".C1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".C1").removeClass("highlight");
+	}
+	else{
+		$(".C1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".C1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".C1-tile").removeClass("highlight");
+	}
+});
+
+$(".D1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".D1").removeClass("highlight");
+	}
+	else{
+		$(".D1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".D1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".D1-tile").removeClass("highlight");
+	}
+});
+
+$(".E1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".E1").removeClass("highlight");
+	}
+	else{
+		$(".E1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".E1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".E1-tile").removeClass("highlight");
+	}
+});
+
+$(".F1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".F1").removeClass("highlight");
+	}
+	else{
+		$(".F1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".F1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".F1-tile").removeClass("highlight");
+	}
+});
+
+$(".G1-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".G1").removeClass("highlight");
+	}
+	else{
+		$(".G1").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".G1").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".G1-tile").removeClass("highlight");
+	}
+});
+
+$(".A2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".A2").removeClass("highlight");
+	}
+	else{
+		$(".A2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".A2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".A2-tile").removeClass("highlight");
+	}
+});
+
+$(".B2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".B2").removeClass("highlight");
+	}
+	else{
+		$(".B2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".B2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".B2-tile").removeClass("highlight");
+	}
+});
+
+$(".C2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".C2").removeClass("highlight");
+	}
+	else{
+		$(".C2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".C2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".C2-tile").removeClass("highlight");
+	}
+});
+
+$(".D2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".D2").removeClass("highlight");
+	}
+	else{
+		$(".D2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".D2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".D2-tile").removeClass("highlight");
+	}
+});
+
+$(".E2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".E2").removeClass("highlight");
+	}
+	else{
+		$(".E2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".E2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".E2-tile").removeClass("highlight");
+	}
+});
+
+$(".F2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".F2").removeClass("highlight");
+	}
+	else{
+		$(".F2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".F2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".F2-tile").removeClass("highlight");
+	}
+});
+
+$(".G2-tile").click(function(){
+	if($(this).hasClass("highlight")){
+		$(".G2").removeClass("highlight");
+	}
+	else{
+		$(".G2").addClass("highlight");
+	}
+	$(this).toggleClass("highlight");
+});
+$(".G2").click(function(){
+	if(!$(this).hasClass("highlight")){
+		$(".G2-tile").removeClass("highlight");
+	}
+});
+//trial
+
+/*$(".A1-tile").click(function(){
 	$(this).toggleClass("highlight");
 	$(".A1").toggleClass("highlight");
 });
@@ -210,4 +422,4 @@ $(".TFF2-tile").click(function(){
 $(".TGG2-tile").click(function(){
 	$(this).toggleClass("highlight");
 	$(".TGG2").toggleClass("highlight");
-});
+});*/

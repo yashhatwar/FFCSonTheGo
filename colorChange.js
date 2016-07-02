6b54e71c67507615c323f8107a4ee55adf4844c8
Sangeeth Sudheer <sangeethmanayil@outlook.com>
2016-07-03 04:40:18 +0530

    Basic list-group functionality, total credits

--- colorChange.js
+++ colorChange.js
@@ -528,20 +528,55 @@ $(".TDD2").click(function(){
  * list and to mark the added slots to the timetable.
  */
 
- $("#slot-sel-area .panel-body button").click(function() {
-	 var slotInput = $("#inputSlotString");
-	 var slot = $("#inputSlotString").val().trim();
-	 if(!slot) {
-		 $("#slot-sel-area .form-group").first().addClass("has-error");
-		 return;
-	 }
-	 var slotArray = slot.split(/\s*\+\s*/);
+(function() {
+	var totalCredits = 0;
 
-	 slotArray.forEach(function(slot) {
-		markSlot(slot.toUpperCase());
-	 });
- });
+	var facultyInput = $("#inputFaculty");
+	var courseInput = $("#inputCourseTitle");
+	var creditsInput = $("#inputCourseCredits");
+	var slotInput = $("#inputSlotString");
+	var totalContainer = $("#slot-sel-area .list-group li.total");
+	var totalSpan = totalContainer.find(".badge");
 
+    $("#slot-sel-area .panel-body button").click(function() {
+			var slot, slotArray, i, normSlotString, li;
+      slot = slotInput.val().trim();
+      if (!slot) {
+          $("#slot-sel-area .form-group").first().addClass("has-error");
+          return;
+      }
+
+      faculty = facultyInput.val().trim();
+      course = courseInput.val().trim();
+      credits = Number(creditsInput.val());
+
+      slotArray = slot.split(/\s*\+\s*/);
+
+      for (i = 0; i < slotArray.length; ++i) {
+          slotArray[i] = slotArray[i].toUpperCase();
+          markSlot(slotArray[i]);
+      }
+
+      normSlotString = slotArray.join(" + ");
+      li = $('<li class="list-group-item">' +
+          '<div class="row">' +
+          '<span class="slots col-sm-3">' + normSlotString + '</span>' +
+          '<span class="course col-sm-5">' + course + '</span>' +
+          '<span class="faculty col-sm-4">' + faculty + '</span>' +
+          '<span class="col-sm-2 text-right">' +
+          '<span class="badge">' + (credits ? credits : 0) + '</span>' +
+          '</span>' +
+          '</div>' +
+          '</li>');
+
+      totalContainer.before(li);
+
+			totalCredits += credits;
+
+			totalSpan.text(totalCredits);
+
+    });
+})();
 /**
  * Toggles slot highlighting of passed slot in the table.
  * @param  {string} slot individual slot obtained from passed input.

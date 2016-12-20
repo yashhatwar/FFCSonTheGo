3bcc903f59a5e4fbe80180051dc3c44912144e4a
VaTz88 <vatz88@gmail.com>
2016-12-20 22:16:16 +0530

    placeholder added courses data updated

--- js/autocomplete_course.js
+++ js/autocomplete_course.js
@@ -1,3 +1,18 @@
+var slotOption = {
+    data: ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "TA1", "TB1", "TC1", "TD1", "TE1", "TF1", "TG1", "TAA1", "TCC1",
+        "A2", "B2", "C2", "D2", "E2", "F2", "G2", "TA2", "TB2", "TC2", "TD2", "TE2", "TF2", "TG2", "TAA2", "TBB2", "TCC2", "TDD2"
+    ],
+    list: {
+        match: {
+            enabled: true
+        }
+    },
+
+    placeholder: "eg: L1+L2+L3",
+
+    theme: "round"
+};
+
 var courseTitleOption = {
     url: "http://vatz88.in/FFCSonTheGo/all_vit_courses.json",
 
@@ -19,6 +34,8 @@ var courseTitleOption = {
             description: "code"
         }
     },
+
+    placeholder: "eg: Computer Networks",
 
     theme: "round"
 };
@@ -46,8 +63,11 @@ var courseCodeOption = {
         }
     },
 
+    placeholder: "eg: ITE208",
+
     theme: "round"
 };
 
 $("#inputCourseTitle").easyAutocomplete(courseTitleOption);
 $("#inputCourseCode").easyAutocomplete(courseCodeOption);
+$("#inputSlotString").easyAutocomplete(slotOption);

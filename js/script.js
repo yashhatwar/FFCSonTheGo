cb978216a204c6e9c7e5c7bad89e851a4a5027d8
Vatsal Joshi <vatz88@gmail.com>
2017-10-27 19:05:50 +0530

    :sparkles: Modal improved - shown only once in 2 days, by setting cookie - modal closes only when user clicks "I understand"

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(function () {
-    // Disclaimer modal config.
-    $("#disclaimer-modal").modal();
+    // Disclaimer modal
+    if (getCookie("disclaimer") !== "shown") {
+        $("#disclaimer-modal").modal({
+            keyboard: false,
+            backdrop: 'static'
+        });
+        setCookie("disclaimer", "shown", 2);
+    }
 
     removeTouchHoverCSSRule();
 
@@ -116,3 +122,26 @@ document.onkeydown = function (e) {
         return true;
     }
 };
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}

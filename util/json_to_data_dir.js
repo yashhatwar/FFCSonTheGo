b5b5c41db03909d8dce5abfa5690df1a506db165
Sangeeth Sudheer <sangeethmanayil@outlook.com>
2018-06-23 18:23:00 +0530

    fix: renamed orig labels to required format

--- util/json_to_data_dir.js
+++ util/json_to_data_dir.js
@@ -2,15 +2,44 @@ const fs = require('fs');
 
 // overwirte the output file
 const result = fs.readFileSync('output.json');
-const resultObj = JSON.parse(result);
+const resultArray = JSON.parse(result);
 
-fs.writeFile(__dirname + "/../data/all_data.json", result, () => {
+const keyMap = {
+    "COURSE\r\nCODE": "CODE",
+    "COURSE NAME": "TITLE",
+    "COURSE\r\nTYPE": "TYPE"
+}
+
+const keysRemove = ["ALLOTED\r\nSEATS"];
+
+for (let courseObj of resultArray) {
+    transformKeys(courseObj, keyMap);
+    removeKeys(courseObj, keysRemove);
+}
+
+fs.writeFile(__dirname + "/../data/all_data.json", JSON.stringify(resultArray), () => {
     console.log('all_data.json updated.');
 });
 
 // remove repeating courses
-unique = resultObj.filter((element, index, self) => self.findIndex(t => t.CODE === element.CODE && t.TITLE === element.TITLE) === index);
+unique = resultArray.filter((element, index, self) => self.findIndex(t => t.CODE === element.CODE && t.TITLE === element.TITLE) === index);
 
 fs.writeFile(__dirname + "/../data/unique_courses.json", JSON.stringify(unique), () => {
     console.log('unique_courses.json updated.');
 });
+
+function transformKeys(obj, keyMap) {
+    const from = Object.keys(keyMap);
+    
+    for(let i = 0; i < from.length; ++i) {
+        let origKey = from[i];
+        
+        const val = obj[origKey];
+        delete obj[origKey];
+        obj[keyMap[origKey]] = val;
+    }
+}
+
+function removeKeys(obj, keys) {
+    for (let key of keys) delete obj[key];
+}
